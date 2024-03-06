29b96d563aba5413abce7369c6ebc2690830f187
Amine <mezzatamine2002@gmail.com>
2024-03-06 06:53:33 +0100

    animationnnssss

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
+import data from "@/app/data/data.json"
+console.log(data)
+
+interface Phone{
+  name : string;
+  id : string;
+}
 
 export default function Home() {
+  let str : string = "mohamed amine mezzat"
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {data.map((item, i) => <h1 key={i}>{item.size}</h1>)}
     </main>
   );
 }
---
+++ app/ui/cards.tsx
@@ -0,0 +1,12 @@
+import next from "next/types"
+import Image from "next/image";
+
+export default function Card(params:type) {
+    let str : string = "amine"
+    return (
+        <section>
+            <h1>{str}</h1>
+            <h2></h2>
+        </section>
+    )
+}
