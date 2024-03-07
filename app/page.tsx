2d83d247d3dd619b63c968f65cccc446f29c0210
Amine <mezzatamine2002@gmail.com>
2024-03-07 10:43:18 +0100

    deleting false data

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   let str : string = "mohamed amine mezzat"
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {data.map((item, i) => <h1 key={i}>{item.size}</h1>)}
+      
     </main>
   );
 }
