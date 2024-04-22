b4808e588d8f1c09b12ca0b6f663139df7f1eac0
Amine <mezzatamine2002@gmail.com>
2024-04-22 21:32:39 +0100

    test

--- app/ui/cards.tsx
+++ app/ui/cards.tsx
@@ -1,12 +1,10 @@
 import next from "next/types"
 import Image from "next/image";
 
-export default function Card(params:type) {
-    let str : string = "amine"
+export default function Card() {
     return (
         <section>
-            <h1>{str}</h1>
-            <h2></h2>
+            
         </section>
     )
 }
