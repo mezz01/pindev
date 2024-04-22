1a38c50f778f13fa0d95f32e5a92fc0b2046a4aa
@Manox96 <aymanemanox15@gmail.com>
2024-04-22 21:28:17 +0100

    test by manox

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import data from "@/app/data/data.json"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <h1>Hello its me Manox</h1>
     </main>
   );
 }
