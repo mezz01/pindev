e96b252aaa26749b6b95b7ea39440a310249c220
Amine <mezzatamine2002@gmail.com>
2024-04-22 21:00:56 +0100

    initialisation

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 import Image from "next/image";
 import data from "@/app/data/data.json"
-console.log(data)
-
-interface Phone{
-  name : string;
-  id : string;
-}
 
 export default function Home() {
-  let str : string = "mohamed amine mezzat"
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
