3efb00bd444c78223782421b7a970170d8d0f21d
Amine <mezzatamine2002@gmail.com>
2024-04-25 09:42:42 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import data from "@/app/data/data.json"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1></h1>
+      <p>
+        
+      </p>
     </main>
   );
 }
